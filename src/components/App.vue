1cac07a9246c76cec2befa6d39cc4f49766521df
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-29 12:08:05 -0400

    add root instance, app component, component template, and bootstrap

---
+++ src/components/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <toolbar></toolbar>
+    <notes-list></notes-list>
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+import Toolbar from './Toolbar.vue'
+import  NotesList from './NotesList'
+import Editor from './Editor'
+
+export default {
+  components: {
+    Toolbar, 
+    NotesList, 
+    Editor
+  }
+}
+</script>
+
+<style>
+html, #app {
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+  height: 100%; 
+}
+body {
+  border: 0; 
+  height: 100%; 
+  margin: 0; 
+  max-height: 100%; 
+  padding: 0; 
+  position: relative; 
+}
+</style>
